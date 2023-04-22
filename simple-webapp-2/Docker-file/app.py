03f508527399e92842311fb8e1c731adf0f8d264
AydinTokuslu <aydintokuslu78@gmail.com>
2023-04-23 00:10:48 +0300

    23.04.23

--- simple-webapp-2/Docker-file/app.py
+++ simple-webapp-2/Docker-file/app.py
@@ -1,32 +1,36 @@
 
+from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
 import os
-from flask import Flask
-from flaskext.mysql import MySQL      # For newer versions of flask-mysql 
-# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
+
+
 app = Flask(__name__)
 
-mysql = MySQL()
 
-mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ['MYSQL_DATABASE_HOST'] or  'localhost'
+mysql_database_host = 'MYSQL_DATABASE_HOST' in os.environ and os.environ[
+    'MYSQL_DATABASE_HOST'] or 'localhost'
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'db_user'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Passw0rd'
 app.config['MYSQL_DATABASE_DB'] = 'employee_db'
 app.config['MYSQL_DATABASE_HOST'] = mysql_database_host
+mysql = MySQL()
 mysql.init_app(app)
+connection = mysql.connect()
+connection.autocommit(True)
+cursor = connection.cursor()
 
-conn = mysql.connect()
-
-cursor = conn.cursor()
 
 @app.route("/")
 def main():
     return "Welcome!"
 
+
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
+
 
 @app.route('/read from database')
 def read():
@@ -34,10 +38,11 @@ def read():
     row = cursor.fetchone()
     result = []
     while row is not None:
-      result.append(row[0])
-      row = cursor.fetchone()
+        result.append(row[0])
+        row = cursor.fetchone()
 
     return ",".join(result)
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
